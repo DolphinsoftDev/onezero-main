26bd5bbc7d0450c2adffe184ef85760b5c8065de
EugeneDolphin <eugeneg@dolphinsoft.co.il>
2024-03-28 18:23:01 +0200

    7 min

--- src/components/FourthScreen/FourthScreen.tsx
+++ src/components/FourthScreen/FourthScreen.tsx
@@ -23,7 +23,7 @@ export default function FourthScreen({}: Props) {
   return (
     <div className="bg-blue flex flex-col pb-12 pt-[4.815rem] px-5 gap-7 text-center text-white items-center md:pb-[6.75rem] md:pt-[5.625rem]">
       <h2 className="font-Digibank-Regular text-[1.5rem] leading-tight md:leading-[1.1] md:text-[2.5rem]">
-        כמה דקות ויש לך חשבון
+         7 דקות ויש לך חשבון
         <br className="hidden md:flex" /> עם <br className="flex md:hidden" />
         חודשיים התנסות ללא עלות.
       </h2>
